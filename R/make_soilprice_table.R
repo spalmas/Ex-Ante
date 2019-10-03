9979407bf573d1174bb7dbe02434b025a04166c7
spalmas <palmasforest@gmail.com>
2019-10-03 15:14:19 +0300

    1000m

--- R/make_soilprice_table.R
+++ R/make_soilprice_table.R
@@ -6,15 +6,15 @@ library(tidyverse)
 ########## SOIL RASTERS ############### 
 #load allrasters and warp to match Organic Carbon layer
 
-gridorc <- rast("data/soil/TZA_ORCDRC_T__M_sd1_250m.tif")
+gridorc <- rast("data/soil/TZA_ORCDRC_T__M_sd1_1000m.tif")
 names(gridorc) <- "gridorc"
+
+gridpH <- rast("data/soil/TZA_PHIHOX_T__M_sd1_1000m.tif") 
+gridacid <- gridpH < 65
+names(gridacid) <- "gridacid"
 
 rain <- rast("data/rainfall/chirps-v2_201512-201605_sum.tif") 
 names(rain) <- "rain"
-
-gridpH <- rast("data/soil/TZA_PHIHOX_T__M_sd1_250m.tif") 
-gridacid <- gridpH < 65
-names(gridacid) <- "gridacid"
 
 acc <- rast("data/mktacc/acc.tif") 
 acc <- warp(acc, gridorc, filename="Data/mktacc/acc_warped.tif", overwrite	= TRUE)
@@ -26,32 +26,27 @@ SPAM <- rast("data/soil/spam2010v1r0_global_physical-area_maiz_a_TZA.tif")
 names(SPAM) <- "SPAM"
 
 #### PRICE RASTERS ####
-urea_price <- rast("Data/input_price/TZA_urea_price.tif") #USD/kg
-urea_price <- warp(urea_price, gridorc, filename="Data/input_price/TZA_urea_price_warped.tif", overwrite	= TRUE)
-names(urea_price) <- "urea_price"
+urea_price <- rast("Data/prices/TZA_urea_price.tif") #USD/kg
+#divide /0.46 to convert to price per kg of N
+N_price <- warp(urea_price/0.46, gridorc, filename="Data/prices/TZA_urea_price_warped.tif", overwrite	= TRUE)
+names(N_price) <- "N_price"
 
-maize_price <- rast("Data/output_price/pred_maize_price1.tif") / 2292 #/2292 to convert to USD/kg
-maize_price <- warp(maize_price, gridorc, filename="Data/input_price/pred_maize_price1_warped.tif", overwrite	= TRUE)
+maize_price <- rast("Data/prices/pred_maize_price1.tif") / 2292 #/2292 to convert to USD/kg
+maize_price <- warp(maize_price, gridorc, filename="Data/prices/pred_maize_price1_warped.tif", overwrite	= TRUE)
 names(maize_price) <- "maize_price"
 
-
-#### FERTILIZER RECOMMENDATION  ####
-AEZ <- rast("data/admin_and_AEZ/tanzania_national_agro_ecological_zones.tiff")
-names(AEZ) <- "AEZ"
-
-#### \\ Admin and AEZ ####
+#### \\ Admin  ####
 gadm36_TZA_1  <- rast("data/admin_and_AEZ/gadm36_TZA_1.tiff")
 
 #### COMBINING RASTERS TO A TABLE ####
 rasters_input <- cbind(values(gadm36_TZA_1),
-                       values(AEZ),
                        values(gridorc),
                        values(gridacid),
                        values(rain),
                        values(acc),
                        values(slope),
                        values(SPAM),
-                       values(urea_price),
+                       values(N_price),
                        values(maize_price)) %>% 
   as_tibble()
 
@@ -59,14 +54,12 @@ rasters_input <- cbind(values(gadm36_TZA_1),
 rasters_input <- rasters_input %>% 
   mutate(index = 1:nrow(rasters_input)) %>% 
   filter(complete.cases(.)) %>% 
-  filter(SPAM>300) %>% 
   mutate(loggridorc = log(gridorc),
          lograin = log(rain),
          accsq = acc^2) %>% 
-  select(index, gadm36_TZA_1, AEZ, lograin, loggridorc, gridacid, acc, accsq, slope, urea_price, maize_price)
+  select(index, gadm36_TZA_1, lograin, loggridorc, gridacid, acc, accsq, slope, N_price, maize_price)
 
 head(rasters_input)
-
 
 #### EXPORTING TABLE TO A FILE  ####
 write.csv(rasters_input, file = paste0('Data/TZA_soilprice_table.csv'))
