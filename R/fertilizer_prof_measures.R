1b5f84572875c975e23951c4f966b06314ad59ec
spalmas <palmasforest@gmail.com>
2019-11-25 17:25:02 +0300

    prof_measures

--- R/fertilizer_prof_measures.R
+++ R/fertilizer_prof_measures.R
@@ -1,32 +1,36 @@
 #' Fertilizer profitability formulas
-#' * Marginal physical product (mp)
-#' * Marginal vlaue cost ratio (mcvr)
 #'
-#' \code{mcvr} 
-#'
-#' @param pixel
+#' @param yield_nf,yield_f yield from of non-fertilized and fertilized areas (kg/ha)
+#' @param N_kgha_nf,N_kgha_f Nitrogen applied in non-fertilized and fertilized areas  (kg/ha)
+#' @param output_price  #farmgate price of crop ($/kg)
+#' @param input_price  #price of N ($/kg)
 #' 
 #' @references  
 #'
 #' @examples
 #' (mp0 <- mp(100,1000,0,30))
 #' mcvr(0.4,mp0, 2)
-mp <- function(yield_f, yield_nf, N_kgha_f, N_kgha_nf){
+
+#Average product
+ap <- function(yield, N_kgha){
+  yield/N_kgha
+}
+
+#Marginal product
+mp <- function(yield_nf, yield_f, N_kgha_nf, N_kgha_f){
   (yield_f-yield_nf)/(N_kgha_f - N_kgha_nf)
 }
 
-ap <- function(yield_f, yield_nf, output_price, N_kgha, input_price){
-  (yield_f-yield_nf)*output_price/(N_kgha*input_price)
+enr <- function(yield_nf, yield_f, output_price, N_kgha_f, input_price){
+  (yield_f-yield_f_nf)*output_price - (N_kgha*input_price)
 }
 
-enr <- function(yield_nf, yield_f, output_price, N_kgha, input_price){
-  (yield_f-yield_nf)*output_price - (N_kgha*input_price)
-}
-
-mcvr <- function(output_price, mp, input_price){
-  output_price*mp/input_price
-}
-
-acvr <- function(output_price, ap, input_price){
+#Average value cost ratio
+avcr <- function(output_price, ap, input_price){
   output_price*ap/input_price
+}
+
+#Marginal value cost ratio
+mvcr <- function(output_price, mp, input_price){
+  output_price*mp/input_price
 }
