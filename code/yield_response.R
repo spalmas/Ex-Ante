9f02f94d9767fac61d3503c1057f216277dc96a1
spalmas <palmasforest@gmail.com>
2020-02-17 10:31:12 +0300

    yield_response

--- code/yield_response.R
+++ code/yield_response.R
@@ -1,20 +1,14 @@
 #' Simulates Fertilizer response to a nitrogen application
 #'
-#' \code{fert_response} Takes a vector of pixel parameters...
+#' \code{fert_response} Takes a vector of pixel parameters. More details in yield_response_fit.Rmd
 #'
-#' @param N applied Nitrogen
-#' @param lograin mm of rain in season (log)
-#' @param loggridorc  
-#' @param gridacid africasoils pH value < 65
-#' @param acc
-#' @param accsq
-#' @param slope
+#' @param N_kgha applied Nitrogen
+#' @param year 2017=1
 #' @param intercrop Binary
 #' @param rotat1 Crop rotation, binary
 #' @param manure Binary
 #' @param cropres Crop residue, Binary
 #' @param weedings Number of weedings, Binary
-#' @param herbicide_n Binary
 #' @param pesticide_n Binary
 #' @param impseed Improved seed, Binary
 #' @param disease Binary
@@ -25,90 +19,77 @@
 #' @param femhead Female head of HH, Binary
 #' @param hhsize Household size, Persons
 #' @param headeduc Years of education of head of HHYears
-#' @param year 2017=1
+#' @param elevation  #meters above sea level
+#' @param slope  #degrees
+#' @param seas_rainfall Seasonal (DEC-MAY) rainfall (mm)
 #' 
 #' @return yield (kg/ha)
 #'
 #' @examples
-#' yield_response(N = 0, lograin = 6.8, loggridorc = 2.6, gridacid = 1,acc =900,slope=2)
-yield_response <- function(N, 
-                           lograin,
-                           loggridorc,
-                           gridacid,
-                           acc,
+#' yield_response(N_kgha = 0, elevation = 100, slope=1, seas_rainfall = 62)
+yield_response <- function(N_kgha, 
+                           year = 2.016262e+03, 
+                           intercrop = 5.250000e-01,   #mean value found in APS Tanzania data
+                           rotat1 = 7.115385e-02,    #mean value found in APS Tanzania data
+                           manure = 1.884615e-01,   #mean value found in APS Tanzania data
+                           cropres = 1.038462e-01,   #mean value found in APS Tanzania data
+                           weedings = 1.846154e+00,   #mean value found in APS Tanzania data
+                           pesticide_bin = 1.923077e-03,   #mean value found in APS Tanzania data
+                           impseed = 1.019231e-01,   #mean value found in APS Tanzania data
+                           disease = 1.365385e-01,   #mean value found in APS Tanzania data
+                           striga = 3.653846e-02,   #mean value found in APS Tanzania data
+                           logha = -5.790308e-01,
+                           headage = 4.742574e+01,   #mean value found in APS Tanzania data
+                           femhead = 1.425743e-01,   #mean value found in APS Tanzania data
+                           hhsize = 5.590385e+00,   #mean value found in APS Tanzania data
+                           headeduc = 7.122772e+00,   #mean value found in APS Tanzania data
+                           elevation,
                            slope,
-                           intercrop = 0.5741935,   #mean value found in APS Tanzania data
-                           rotat1 = 0.0840868,    #mean value found in APS Tanzania data
-                           manure = 0.1952984,   #mean value found in APS Tanzania data
-                           cropres = 0.0786618,   #mean value found in APS Tanzania data
-                           weedings = 1.685353,   #mean value found in APS Tanzania data
-                           herbicide_n = 0.0027125,   #mean value found in APS Tanzania data
-                           pesticide_n = 0.0072333,   #mean value found in APS Tanzania data
-                           impseed = 0.1925859,   #mean value found in APS Tanzania data
-                           disease = 0.1229656,   #mean value found in APS Tanzania data
-                           striga = 0.0271248,   #mean value found in APS Tanzania data
-                           logha = -0.6459806,   #mean value found in APS Tanzania data
-                           headage = 48.61806,   #mean value found in APS Tanzania data
-                           headagesq = 2563.171,   #mean value found in APS Tanzania data
-                           femhead = 0.1333333,   #mean value found in APS Tanzania data
-                           hhsize = 5.670886,   #mean value found in APS Tanzania data
-                           headeduc = 7.221596,   #mean value found in APS Tanzania data
-                           year = 1) {    #if year is 2017
+                           seas_rainfall) {    #if year is 2017
+  
   #### Parameters for each variable  ####
-  p_N <- 10.87
-  p_NN <- -0.00942
-  p_lograin <- 692.8
-  p_intercrop <- -408.9
-  p_rotat1 <- -294.4
-  p_manure <- 465.8
-  p_cropres <- -296.1
-  p_weedings <- -31.94
-  p_herbicide_n <- -303.6
-  p_pesticide_n <- -2541
-  p_impseed <- 714.5
-  p_disease <- -118.4
-  p_striga <- 46.53
-  p_logha <- 247.9
-  p_headage <- -9.239
-  p_headagesq <- 0.0352
-  p_femhead <- 0.0176
-  p_hhsize <- 31.12
-  p_headeduc <- -11.98
-  p_loggridorc <- 550.8
-  p_gridacid <- -315.5
-  p_acc <- 11.82
-  p_accsq <- -0.00638
-  p_slope <- -65.92
-  p_year <- 465.6
-  constant <- -8228
+  intercept <- -1125632
+  p_N_kgha <- 2.297359 
+  p_year <- 558.4256   
+  p_intercrop <- 96.5243 
+  p_rotat1 <- -606.839
+  p_manure <- 296.5286 
+  p_cropres <- -106.0565
+  p_weedings <- -21.68373 
+  p_pesticide_bin <- -2374.574
+  p_impseed <- 1437.885
+  p_disease <- -211.2318
+  p_striga <- 223.6319 
+  p_logha <- 147.6604 
+  p_headage <- -2.40868
+  p_femhead <- -143.7437
+  p_hhsize <- 36.73523
+  p_headeduc <- 0.3248557 
+  p_elevation <- 0.3986424 
+  p_slope <- -125.503 
+  p_seas_rainfall <- 2.238996
   
   #yield calculation
-  yield <- N*p_N + 
-    N^2*p_NN+
-    lograin*p_lograin +
+  yield <- intercept + 
+    N_kgha*p_N_kgha+
+    year*p_year +
     intercrop*p_intercrop +
     rotat1*p_rotat1 + 
     manure*p_manure +
     cropres*p_cropres + 
     weedings*p_weedings + 
-    herbicide_n*p_herbicide_n + 
-    pesticide_n*p_pesticide_n + 
+    pesticide_bin*p_pesticide_bin + 
     impseed*p_impseed + 
     disease*p_disease + 
     striga*p_striga + 
     logha*p_logha + 
     headage*p_headage + 
-    headagesq*p_headagesq + 
     femhead*p_femhead + 
     hhsize*p_hhsize + 
     headeduc*p_headeduc + 
-    loggridorc*p_loggridorc + 
-    gridacid*p_gridacid + 
-    acc*p_acc + 
-    acc^2*p_accsq + 
-    slope*p_slope + 
-    year*p_year +
-    constant
+    elevation*p_elevation + 
+    slope*p_slope +
+    seas_rainfall*p_seas_rainfall
   
   #do not allow negative values.
   if (yield<0) yield <- 0
